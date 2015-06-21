c8e25f736ba233a04ecb93c0b4ebe6eb57f64520
Tarek S Hafez <thafez@ymail.com>
2015-06-20 22:00:52 -0500

    Modified javascript_challenge1.html and js/array_reverseArray_challenge_js.js

--- public/js/array_reverseArray_challenge_js.js
+++ public/js/array_reverseArray_challenge_js.js
@@ -1,48 +1,15 @@
 "use strict";
 (function () {
-	var array = [];
-
-	function reverseArray() {
-			
-		for (var i = 0; i < 6; i++) {
-			var number = i;
-			function init(i) {
-				var initArray = (i % 2 == 0) ? "Array:" : "Reverse Array:";				
-				console.log(initArray)
-			}
-			function arrayRev(number) {
-				// var Splice = array.splice(0, 3, "c", "b", "a");
-
-				if (number == 0) {
-					array.unshift("a");
-					console.log(array);
-				} else if (number == 1) {
-					console.log(array);
-				} else if (number == 2) {
-					array.push("b", "c");
-					console.log(array);
-				} else if (number == 3) {
-					array.shift();
-					array.pop();
-					array.push("a");
-					array.unshift("c");
-					console.log(array);
-				} else if (number == 4) {
-					array.shift();
-					array.pop();
-					array.unshift("a")
-					array.push("c", "d", "e")
-					console.log(array);
-				} else if (number == 5) {
-					array.splice(array.indexOf("a"), 5, "e", "d", "c", "b", "a");
-					console.log(array);
-				}
-			}
-
-			init(i);
-			arrayRev(i);
-			}
+	function reverseArray(array) {
+		for (var i = 0; i < array.length; i++) {
+			array.splice(0, 0, array[i]);
+			array.splice((i + 1), 1);
+		};
+		console.log(array);
+		
 	}
 
-	reverseArray()
+	var test1 = ['a', 'b', 'c'];
+
+	var test1Reverse = reverseArray(test1)
 })();
