7e9ebfaa7c5db7dca6f070b9d167acaa265cb2c7
Tarek S Hafez <thafez@ymail.com>
2015-07-21 14:21:04 -0500

    Adding ping.php and pong.php

---
+++ public/ping.php
@@ -0,0 +1,29 @@
+<?php
+    var_dump($_GET);
+
+    if (isset($_GET['hit'])){
+        $hitCounter = $_GET['hit'];
+        if (!empty($_GET)) {
+            if ($_GET['miss'] == 'no') {
+                $hitCounter += 1;
+            } else if ($_GET['miss'] == 'yes') {
+                $hitCounter = 0;
+            }
+        }
+    } else {
+        $hitCounter = 0;
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ping</title>
+</head>
+<body>
+    <h2>Score: <?= $hitCounter ?></h2>
+    <a href="/pong.php?hit=<?= $hitCounter ?>&miss=no">Hit</a>
+    <a href="?miss=yes">Miss</a>
+</body>
+</html>
---
+++ public/pong.php
@@ -0,0 +1,31 @@
+<?php
+    
+    
+    var_dump($_GET);
+
+    if (isset($_GET['hit'])){
+        $hitCounter = $_GET['hit'];
+        if (!empty($_GET)) {
+            if ($_GET['miss'] == 'no') {
+                $hitCounter += 1;
+            } else if ($_GET['miss'] == 'yes') {
+                $hitCounter = 0;
+            }
+        }
+    } else {
+        $hitCounter = 0;
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pong</title>
+</head>
+<body>
+    <h2>Score: <?= $hitCounter ?></h2>
+    <a href="/ping.php?hit=<?= $hitCounter ?>&miss=no">Hit</a>
+    <a href="?miss=yes">Miss</a>
+</body>
+</html>
