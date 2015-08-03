b5ae3d1e7780aaea712d17eaac34986a48c5ffa6
Tarek S Hafez <thafez@ymail.com>
2015-08-03 09:54:53 -0500

    Modified park_seeder.php to correct error in variable name adjustments...got a bit too overzealous...

--- park_seeder.php
+++ park_seeder.php
@@ -11,6 +11,7 @@
     $dbc->exec($truncateSql);
 
     $parks = [
+    ['name' => 'Glacier', 'location' => 'Montana', 'date_established' => '1910/05/11', 'area_in_acres' => 1013572.41],
     ['name' => 'Acadia', 'location' => 'Maine', 'date_established' => '1919/02/26', 'area_in_acres' => 47389.67],
     ['name' => 'Arches', 'location' => 'Utah', 'date_established' => '1929/04/12', 'area_in_acres' => 76518.98],
     ['name' => 'Badlands', 'location' => 'South Dakota', 'date_established' => '1978/11/10', 'area_in_acres' => 242755.94 ],
@@ -19,12 +20,13 @@
     ['name' => 'Black Canyon of the Gunnison', 'location' => 'Colorado', 'date_established' => '1999/10/21', 'area_in_acres' => 32950.03],
     ['name' => 'Canyonlands', 'location' => 'Utah', 'date_established' => '1964/08/12', 'area_in_acres' => 337597.83],
     ['name' => 'Capitol Reef', 'location' => 'Utah', 'date_established' => '1971/12/18', 'area_in_acres' => 241904.26],
-    ['name' => 'Carlsbad Caverns', 'location' => 'New Mexico', 'date_established' => '1930/05/14', 'area_in_acres' => 46766.45],
-    ['name' => 'Glacier', 'location' => 'Montana', 'date_established' => '1910/05/11', 'area_in_acres' => 1013572.41]
+    ['name' => 'Carlsbad Caverns', 'location' => 'New Mexico', 'date_established' => '1930/05/14', 'area_in_acres' => 46766.45]
     ];
 
     foreach ($parks as $details => $park) {
         $insertSql = "INSERT INTO national_parks (name, location, date_established, area_in_acres) VALUES ('{$park['name']}', '{$park['location']}', '{$park['date_established']}', '{$park['area_in_acres']}')";
-        $numRow->exec($insertSql);
-        echo "Inserted ID: " . $numRow->lastInsertId() . PHP_EOL;
+
+        $dbc->exec($insertSql);
+
+        echo "Inserted ID: " . $dbc->lastInsertId() . PHP_EOL;
     }
